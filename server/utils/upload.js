8256447761394f237845936ad1ced6e1b192d716
MrGold441 <prajjwalkumar2004@gmail.com>
2024-08-07 20:43:54 +0530

    Final Changes

--- server/utils/upload.js
+++ server/utils/upload.js
@@ -1,14 +1,22 @@
 import multer from 'multer';
+import dotenv from 'dotenv';
 import { GridFsStorage } from 'multer-gridfs-storage';
 
+dotenv.config();
+
+
+const USERNAME = process.env.DB_USERNAME;
+const PASSWORD = process.env.DB_PASSWORD;
+
+
 const storage = new GridFsStorage({
-    url: `mongodb://user:codeforinterview@blogweb-shard-00-00.ch1hk.mongodb.net:27017,blogweb-shard-00-01.ch1hk.mongodb.net:27017,blogweb-shard-00-02.ch1hk.mongodb.net:27017/BLOG?ssl=true&replicaSet=atlas-lhtsci-shard-0&authSource=admin&retryWrites=true&w=majority`,
+    url: `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.qti5rpo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
     options: { useNewUrlParser: true },
     file: (request, file) => {
         const match = ["image/png", "image/jpg"];
 
-        if(match.indexOf(file.memeType) === -1) 
-            return`${Date.now()}-blog-${file.originalname}`;
+        if (match.indexOf(file.memeType) === -1)
+            return `${Date.now()}-blog-${file.originalname}`;
 
         return {
             bucketName: "photos",
@@ -17,4 +25,4 @@ const storage = new GridFsStorage({
     }
 });
 
-export default multer({storage}); 
+export default multer({ storage }); 
